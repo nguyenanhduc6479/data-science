ec7927dce46bcd19fec06dee2e5d4111f2a5d0c6
nguyenanduc <105762377+nguyenanhduc6479@users.noreply.github.com>
2022-08-23 15:30:59 +0700

    Update spacex_dash.py

--- spacex_dash.py
+++ spacex_dash.py
@@ -1,4 +1,4 @@
-# Import required libraries
+
 import pandas as pd
 import dash
 from dash import html
@@ -6,22 +6,18 @@ from dash import dcc
 from dash.dependencies import Input, Output
 import plotly.express as px
 
-# Read the airline data into pandas dataframe
+
 spacex_df = pd.read_csv("spacex_launch_dash.csv")
 max_payload = spacex_df['Payload Mass (kg)'].max()
 min_payload = spacex_df['Payload Mass (kg)'].min()
 
-#print(spacex_df['Launch Site'].unique())
 
-# Create a dash application
 app = dash.Dash(__name__)
 
-# Create an app layout
 app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                         style={'textAlign': 'center', 'color': '#503D36',
                                                'font-size': 40}),
-                                # TASK 1: Add a dropdown list to enable Launch Site selection
-                                # The default select value is for ALL sites
+
                                 dcc.Dropdown(id='site-dropdown',
                                              options=[
                                                     {'label': 'All Sites', 'value': 'ALL'},
@@ -34,26 +30,16 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                              searchable=True
                                             ),
                                 html.Br(),
-
-                                # TASK 2: Add a pie chart to show the total successful launches count for all sites
-                                # If a specific launch site was selected, show the Success vs. Failed counts for the site
                                 html.Div(dcc.Graph(id='success-pie-chart')),
                                 html.Br(),
 
                                 html.P("Payload range (Kg):"),
-                                # TASK 3: Add a slider to select payload range
                                 dcc.RangeSlider(id='payload-slider',
                                                 min=0, max=10000, step=1000,
                                                 marks={0: '0', 100: '100'},
                                                 value=[min_payload, max_payload]),
-
-                                # TASK 4: Add a scatter chart to show the correlation between payload and launch success
                                 html.Div(dcc.Graph(id='success-payload-scatter-chart')),
                                 ])
-
-# TASK 2:
-# Add a callback function for `site-dropdown` as input, `success-pie-chart` as output
-# Function decorator to specify function input and output
 @app.callback(Output(component_id='success-pie-chart', component_property='figure'),
               Input(component_id='site-dropdown', component_property='value'))
 def get_pie_chart(entered_site):    
@@ -69,11 +55,7 @@ def get_pie_chart(entered_site):
                      values='Unnamed: 0', 
                      names='class', 
                      title='Total Launches for site {}'.format(entered_site))        
-        # return the outcomes piechart for a selected site
     return fig
-        
-# TASK 4:
-# Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
 @app.callback(Output(component_id='success-payload-scatter-chart', component_property='figure'),
               Input(component_id='site-dropdown', component_property='value'), 
               Input(component_id="payload-slider", component_property="value"))
@@ -92,7 +74,5 @@ def get_scatter_chart(entered_site, payload_range):
         fig = px.scatter(filtered_df, x='Payload Mass (kg)', y='class', color='Booster Version Category', title='Site {} - payload mass between {:8,d}kg and {:8,d}kg'.format(entered_site,int(payload_range[0]),int(payload_range[1])))
     
     return fig
-
-# Run the app
 if __name__ == '__main__':
     app.run_server()
